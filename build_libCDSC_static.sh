bc15f9c92a9b9736787dc50e4aad32ee091bc5f1
assyrianic <assyrianic@users.noreply.github.com>
2018-05-26 17:00:11 -0500

    Add files via upload

--- build_libCDSC_static.sh
+++ build_libCDSC_static.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-gcc	-Wall -Wextra -std=c99 -O666 -c 	stringobj.c vector.c hashmap.c unilist.c bilist.c bytebuffer.c tuple.c heap.c graph.c tree.c
-ar	cr libDataStructColl.a		stringobj.o vector.o hashmap.o unilist.o bilist.o bytebuffer.o tuple.o heap.o graph.o tree.o
+gcc	-Wall -Wextra -std=c99 -O666 -c 	stringobj.c vector.c hashmap.c unilist.c bilist.c bytebuffer.c tuple.c heap.c graph.c tree.c pluginarch.c
+ar	cr libDataStructColl.a		stringobj.o vector.o hashmap.o unilist.o bilist.o bytebuffer.o tuple.o heap.o graph.o tree.o pluginarch.o
 
-rm	stringobj.o vector.o hashmap.o unilist.o bilist.o bytebuffer.o tuple.o heap.o graph.o tree.o
+rm	stringobj.o vector.o hashmap.o unilist.o bilist.o bytebuffer.o tuple.o heap.o graph.o tree.o pluginarch.o
