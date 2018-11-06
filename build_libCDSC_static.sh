bb42df7933835a47c4eaa30f3211f4ec77bbae41
assyrianic <assyrianic@users.noreply.github.com>
2018-11-06 14:40:52 -0700

    Delete build_libCDSC_static.sh

--- build_libCDSC_static.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-gcc	-Wall -Wextra -std=c99 -O3 -c 	stringobj.c vector.c hashmap.c unilist.c bilist.c bytebuffer.c tuple.c heap.c graph.c tree.c pluginarch.c linkmap.c
-ar	cr libDataStructColl.a		stringobj.o vector.o hashmap.o unilist.o bilist.o bytebuffer.o tuple.o heap.o graph.o tree.o pluginarch.o linkmap.o
-
-rm	stringobj.o vector.o hashmap.o unilist.o bilist.o bytebuffer.o tuple.o heap.o graph.o tree.o pluginarch.o linkmap.o
