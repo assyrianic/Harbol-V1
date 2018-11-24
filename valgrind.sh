1e53741456ce2d811f9903ab9499c16f23162c92
assyrianic <assyrianic@users.noreply.github.com>
2018-11-23 17:56:17 -0700

    Add files via upload

--- valgrind.sh
+++ valgrind.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
+
+gcc -Wall -Wextra -std=c99 -g -O2 stringobj.c vector.c hashmap.c unilist.c bilist.c bytebuffer.c tuple.c mempool.c graph.c tree.c linkmap.c variant.c cfg.c test_suite.c -o harbol_testprogram
+
 valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -v ./harbol_testprogram
